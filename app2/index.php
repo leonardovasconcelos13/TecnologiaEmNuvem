545277228c138915863de13d60ddb15d911765a3
leonardovasconcelos13 <leonardo.sa35@etec.sp.gov.br>
2025-09-15 09:14:42 -0300

    Update index.php

--- app2/index.php
+++ app2/index.php
@@ -1,24 +1,20 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-  <meta charset="UTF-8">
-  <title>Formulário de Contato</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Contato</title>
 </head>
 <body>
-  <h1>Fale Conosco</h1>
+    <h1>Fale Conosco</h1>
 
-  <form method="post" action="">
-    <label for="nome">Nome</label><br>
-    <input type="text" name="nome" required><br><br>
+    <form method="post" action="">
+        <label for="nome">Nome:</label><br>
+        <input type="text" name="nome" id="nome" required><br><br>
 
-    <label for="email">Email</label><br>
-    <input type="email" name="email" required><br><br>
-
-    <label for="mensagem">Mensagem</label><br>
-    <textarea name="mensagem" id="mensagem" rows="5" required></textarea><br><br>
-
-    <input type="submit" value="Enviar">
-  </form>
-<?php
+        <label for="email">Email:</label><br>
+        <input type="email" name="email" id="email" required><br><br>
+    </form>
 </body>
-  </html>
+</html>
