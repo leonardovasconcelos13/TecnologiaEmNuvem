b98889563dea4d26b1b61e515a863387fb567406
leonardovasconcelos13 <leonardo.sa35@etec.sp.gov.br>
2025-09-23 10:07:25 -0300

    Update index.php

--- app3/index.php
+++ app3/index.php
@@ -1,23 +1,22 @@
 <?php
-
-// Função para converter uma string para binario
+// Função para conbverter uma string para binário
 function stringParaBinario($string) {
-// A função str_split divide a string em uma array de caracteres
-// A função ord pega o valor ASCII em binário
+  // A função str_split  divide a string em um array de caracteres
+  // A função ord pega o valor ASCII de cada caractere
+  // A função decbin converte o valor ASCII em binário
   $binario = '';
-  foreach(str_split($string) as $char) {
-  //Converte o caractere para o valor binario de 8 bits
-    $binario = str_pad(decbin(ord($char)) '8', '0', STR_PAD_LEFT) , '';
+  foreach (str_split($string) as $char) {
+    // Converte o caractere para o valor binário de 8 bits
+    $binario .= str_pad(decbin(ord($char)), 8, '0', STR_PAD_LEFT) . ' ';
   }
   return $binario;
 }
+// Exemplo de uso
+$frase = "Pedro Bueno de Souza";
+$fraseBinario = stringParaBinario ($frase);
 
-//Exemplo de uso
-$frase = "Olá Mundo!";
-$fraseBinario = stringParaBinario($frase);
-
-//Exibido a frase original e sua versão ninaria
-echo "Frase original: ", $frase , "<br>";
-echo "Frase em binário: " , $fraseBinario;
-
+// Exibindo a frase original e sua versão binária
+echo "Frase original: " . $frase . "<br>";
+echo "Frase em binário: " . $fraseBinario;
 ?>
+ 
